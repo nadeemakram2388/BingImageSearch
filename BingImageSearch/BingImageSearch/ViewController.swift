d2993a7b7aba9ca4e6ac249f9a20c355c25ecc03
nadeem-talentedge <nadeem.akram@talentedge.in>
2020-01-30 11:09:13 +0530

    Cocoapods setup with MBProgressHUD, AFNetworking and Mantle.

---
+++ BingImageSearch/BingImageSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingImageSearch
+//
+//  Created by Nadeem Akram on 30/01/20.
+//  Copyright © 2020 Nadeem Akram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
